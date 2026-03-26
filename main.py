2cdd0159a6a2833b5975222a84f7d9faf293c6e7
Michal. <mieliano26@gmail.com>
2026-03-27 01:17:09 +0200

    Integrate Flask app and main execution logic

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)  # השורה הזו חסרה לך!
+
+# כאן מגיע כל שאר הקוד שסטיץ' נתן לך...
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
 import os
 import sys
 
